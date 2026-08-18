d7f2950777567138bf4eed32562770ea58fef4ba
DarkFox90 <carlospozuelos3@gmail.com>
2026-08-17 22:48:20 -0600

    agregado borde, texto y modificado sdk

--- lab04/app/src/main/java/com/example/lab1/lab04/MainActivity.kt
+++ lab04/app/src/main/java/com/example/lab1/lab04/MainActivity.kt
@@ -4,13 +4,27 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.lab1.lab04.ui.theme.Lab04Theme
 
 class MainActivity : ComponentActivity() {
@@ -18,10 +32,9 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            Lab04Theme {
+            Lab04Theme(darkTheme = false, dynamicColor = false) {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    Cuadro1(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -37,11 +50,44 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
         modifier = modifier
     )
 }
+@Composable
+fun Cuadro1(
+    modifier: Modifier = Modifier
+) {
+   Box(
+       modifier = modifier
+   ) {
+       Box(
+           modifier = Modifier
+               .fillMaxSize()
+               .border(10.dp, Color.Green)
+
+       ) {
+           Column(
+               modifier = Modifier
+                   .fillMaxSize(),
+               horizontalAlignment = Alignment.CenterHorizontally,
+               verticalArrangement = Arrangement.Center
+
+           ) {
+               Text(
+                   text = "Universidad del Valle de Guatemala",
+                   fontWeight = FontWeight.Bold,
+                   fontSize = 24.sp
+               )
+           }
+       }
+
+   }
+}
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     Lab04Theme {
-        Greeting("Android")
+        Cuadro1(
+            modifier = Modifier
+                .fillMaxSize()
+        )
     }
 }
