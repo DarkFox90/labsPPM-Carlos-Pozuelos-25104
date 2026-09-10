0db49e6daafff87ccb659e81708c8bfbaaaeac67
DarkFox90 <carlospozuelos3@gmail.com>
2026-09-09 18:04:02 -0600

    añadido el historial

--- lab06/app/src/main/java/com/example/lab1/lab06/MainActivity.kt
+++ lab06/app/src/main/java/com/example/lab1/lab06/MainActivity.kt
@@ -4,16 +4,19 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.animation.animateContentSize
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.FilledIconButton
-import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -26,11 +29,12 @@ import com.example.lab1.lab06.ui.theme.Lab06Theme
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
-import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.compose.foundation.lazy.grid.items
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -131,8 +135,48 @@ fun PantallaContador (modifier: Modifier = Modifier) {
             Estadisticas(etiqueta = "Total decrementos:", valor = decrementosTotales)
             Estadisticas(etiqueta = "Valor máximo:", valor = valorMax)
             Estadisticas(etiqueta = "Total minimo:", valor = valorMin)
-
             Estadisticas(etiqueta = "Total cambios:", valor = incrementosTotales + decrementosTotales)
+        }
+
+        Text(
+            text = "Historial:",
+            fontSize = 20.sp,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier
+                .align(Alignment.Start)
+        )
+
+        LazyVerticalGrid(
+            columns = GridCells.Fixed(5),
+            modifier = Modifier
+                .fillMaxWidth()
+                .weight(1f),
+            verticalArrangement = Arrangement.spacedBy(8.dp),
+            horizontalArrangement = Arrangement.spacedBy(8.dp)
+        ) {
+            items(historial) { pareja ->
+                Box(
+                    modifier = Modifier
+                        .size(46.dp)
+
+                        .background (
+                            if (pareja.second) {
+                                Color(0xFF2E7D32)
+                            } else {
+                                Color(0xFFC62828)
+                            },
+                            shape = RoundedCornerShape(8.dp)
+                        ),
+                    contentAlignment = Alignment.Center
+                ) {
+                    Text(
+                        text = "${pareja.first}",
+                        color = Color.White,
+                        fontSize = 24.sp,
+                        fontWeight = FontWeight.Bold
+                    )
+                }
+            }
         }
     }
 }
